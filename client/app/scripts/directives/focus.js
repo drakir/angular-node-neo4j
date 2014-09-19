dbf557d752d3e36863473afec619fea4bf62e16f
Rikard Lindstrom <rikard.lindstrom@cygni.se>
2014-09-19 10:15:57 +0200

    directive to make an editible cell

---
+++ client/app/scripts/directives/focus.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name clientApp.directive:focus
+ * @description
+ * # focus
+ */
+angular.module('clientApp')
+    .directive('focus', function ($timeout) {
+        return {
+            restrict: 'A',
+            scope: { trigger: '@focus' },
+            link: function (scope, element) {
+                scope.$watch('trigger', function (value) {
+                    if (value === "true") {
+                        $timeout(function () {
+                            element[0].focus();
+                        });
+                    }
+                });
+            }
+        }
+    });
